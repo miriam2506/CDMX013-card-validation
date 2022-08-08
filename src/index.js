d36f486c00321aab22151e7c407efb4395ccb2cc
Miriam Cortes <miriamcortes.06@gmail.com>
2022-08-08 14:21:19 -0500

    subir cambios a github

--- src/index.js
+++ src/index.js
@@ -1,3 +1,61 @@
 import validator from './validator.js';
+const btn = document.getElementById('donar');
+const input= document.getElementById('cardNumber');
 
-console.log(validator);
+btn.addEventListener ('click', function (e){
+e.preventDefault()
+
+if (validator.isValid (input.value)===true){
+  input.value= validator.maskify(input.value)
+  console.log ("válida")
+ document.getElementById('donar').innerHTML = "<span style='color: green;'>'DONACIÓN EXITOSA'</span>";
+//alert ("Válida");
+}
+  else{
+    console.log ("Rechazada")
+     document.getElementById('donar').innerHTML = "<span style='color: red;'>'VUELVE A INTENTARLO'</span>";
+    
+     }
+   
+    });
+ //alert ("Tarjeta rechazada");
+// input.addEventListener('keyup',function (e){
+//e.preventDefault()
+  
+// })
+
+ 
+
+ /*function soloNumeros(e) {
+
+    key = e.keyCode || e.which;
+    teclado = String.fromCharCode(key);
+    numeros = "0,1,2,3,4,5,6,7,8,9"
+    especiales = "8-37-38-46";
+    teclado_especial = false;
+    for (var i in especiales) {
+        if (key == especiales[i]) {
+            teclado_especial = true
+        }
+        if (numeros.indexOf(teclado) == -1 && !teclado_especial) {
+            return false;
+        }
+    }
+}
+    
+}
+*/
+
+
+
+
+//validador.addEventListener('donar',function(){
+
+  //)
+
+
+/*
+  */
+
+
+
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,74 @@
 const validator = {
-  // ...
-};
+    isValid: function (creditCardNumber) {
+
+        console.log(creditCardNumber)
+        //INVERTIR VALORES
+        //arreglo para separar valores
+        const cardNumberArray = creditCardNumber.split('');
+        //invertir valores
+        const cardNumberInverse = cardNumberArray.reverse();
+        //convertirlo a string otra vez
+        const cardNumberConvert = cardNumberInverse.join('');
+        console.log(cardNumberConvert)
+
+        for (let i = 0; i < cardNumberConvert.length; i++) {
+            console.log(cardNumberConvert.length)
+            let resultado = 0;
+            if (i % 2 !== 0) {
+                //multiplicar por dos
+                console.log('Sí entró')
+                const multiplicar = (cardNumberConvert[i]*2);
+                console.log(multiplicar)
+                
+                // si es mayor a 9 sumar dígitos 
+                if (multiplicar >= 10) {
+                    const dig1 = String(multiplicar)[0]
+                    const dig2 = String(multiplicar)[1]
+                    const suma = Number(dig1) + Number(dig2);
+
+                    resultado += Number(suma)
+                }
+                else {
+                    resultado += Number(multiplicar)
+                }
+            }
+            else {
+                resultado += Number(cardNumberConvert[i])
+            }
+console.log(resultado)
+            if (resultado%10===0) {
+                return true
+            }
+            else {
+                return false
+
+            }
+        }
+    },
+
+    maskify: function (cardNumberInput) {
+
+        if (cardNumberInput.length <= 4) {
+            return cardNumberInput
+        }
+
+        else {
+            let guardar = cardNumberInput.slice(-4)
+            let masked = cardNumberInput.slice(0, -4)
+            let gato = ""
+            for (let contador = 0; contador < masked.length; contador++) {
+                gato += "#"
+            }
+
+            console.log(gato + guardar)
+            return gato + guardar
+        }
+
+
+    }
+
+
+
+}
 
 export default validator;
