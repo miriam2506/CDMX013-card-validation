e61e6dcb2b57e4f7a469b28bd57437e7f732ab44
Miriam Cortes <miriamcortes.06@gmail.com>
2022-08-08 20:11:55 -0500

    FINAL

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ if (validator.isValid (input.value)===true){
   document.getElementById('donar').innerHTML = "<span style='color: green;'>'DONACIÓN EXITOSA'</span>";
 }
   else{
-    
+
          document.getElementById('donar').innerHTML = "<span style='color: red;'>'VUELVE A INTENTARLO'</span>";
          }
          input.value= validator.maskify(input.value);
@@ -18,44 +18,4 @@ if (validator.isValid (input.value)===true){
     
   
     
- //alert ("Tarjeta rechazada");
-// input.addEventListener('keyup',function (e){
-//e.preventDefault()
-  
-// })
-
- 
-
- /*function soloNumeros(e) {
-
-    key = e.keyCode || e.which;
-    teclado = String.fromCharCode(key);
-    numeros = "0,1,2,3,4,5,6,7,8,9"
-    especiales = "8-37-38-46";
-    teclado_especial = false;
-    for (var i in especiales) {
-        if (key == especiales[i]) {
-            teclado_especial = true
-        }
-        if (numeros.indexOf(teclado) == -1 && !teclado_especial) {
-            return false;
-        }
-    }
-}
-    
-}
-*/
-
-
-
-
-//validador.addEventListener('donar',function(){
-
-  //)
-
-
-/*
-  */
-
-
 
